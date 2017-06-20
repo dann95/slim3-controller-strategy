1dd1e5f840ff79c3813945457f7b17ad3abd3f29
= <danielrubin95@hotmail.com>
2017-06-20 15:24:45 -0300

    some assert validation for security..

--- src/CallableResolver.php
+++ src/CallableResolver.php
@@ -5,6 +5,7 @@ namespace Dann95\SlimController;
 
 use Slim\Interfaces\CallableResolverInterface;
 use Dann95\SlimController\Contracts\ControllerSolver;
+use RuntimeException;
 
 class CallableResolver implements CallableResolverInterface
 {
@@ -31,6 +32,36 @@ class CallableResolver implements CallableResolverInterface
         if(is_callable($toResolve))
             return $toResolve;
 
-        return $this->solver->solve($toResolve);
+        $solved = $this->solver->solve($toResolve);
+
+        $this->assetArray($solved);
+        $this->assertCallableMethod($solved);
+
+        return $solved;
+    }
+
+    /**
+     * @param $solved
+     */
+    private function assetArray($solved)
+    {
+        if(! (is_array($solved) || count($solved) == 2))
+            throw new RuntimeException(sprintf(
+                '%s is not resolvable',
+                is_array($solved) ? json_encode($solved) : $solved
+            ));
+    }
+
+    /**
+     * @param $solved
+     */
+    private function assertCallableMethod($solved)
+    {
+        if(! method_exists($solved[0], $solved[1]))
+            throw new RuntimeException(sprintf(
+                'The method %s does exist in the class %s',
+                $solved[1],
+                get_class($solved[0])
+            ));
     }
 }
